2e4058f0cd7005aa1474c2ecd60bada0b9473c57
Bartosz Puszkarski <bartosz.puszkarski@addepto.com>
2024-10-31 20:19:53 +0100

    ndarrays to list

--- utilities/results_handling.py
+++ utilities/results_handling.py
@@ -4,14 +4,14 @@ import json
 
 class ResultHandler:
     def __init__(self, c=None, binary_outputs=None, scalar_outputs=None, times=None, auroc=None, auprc=None, auroc_classes=None, auprc_classes=None, f_measure=None, f_measure_classes=None, challenge_metric=None) -> None:
-        self.c = c
-        self.binary_outputs=binary_outputs
-        self.scalar_outputs=scalar_outputs
-        self.times=times
-        self.auroc=auroc
-        self.auprc=auprc
-        self.auroc_classes=auroc_classes
-        self.auprc_classes=auprc_classes
+        self.c = c.toList()
+        self.binary_outputs=binary_outputs.toList()
+        self.scalar_outputs=scalar_outputs.toList()
+        self.times=times.toList()
+        self.auroc=auroc.toList()
+        self.auprc=auprc.toList()
+        self.auroc_classes=auroc_classes.toList()
+        self.auprc_classes=auprc_classes.toList()
         self.f_measure=f_measure
         self.f_measure_classes=f_measure_classes
         self.challenge_metric=challenge_metric
