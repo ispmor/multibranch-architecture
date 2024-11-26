4011523af338f3c4b0c1dba1c69de6fdee52e309
Bartosz Puszkarski <bartosz.puszkarski@addepto.com>
2024-11-26 15:51:47 +0100

    adding some spaces

--- run_fold_per_process.sh
+++ run_fold_per_process.sh
@@ -5,7 +5,11 @@ NOHUP="nohup_with_bw_multi2.out"
 DATASET_DIR="h5_datasets/multi2_with_bw/"
 
 nohup python3 main.py -i $DATA_DIR -g $GPU -t $DATASET_DIR -n "${EXPERIMENT_NAME}_fold_0 -f "0" &> ${NOHUP}_fold_0" &
+
 nohup python3 main.py -i $DATA_DIR -g $GPU -t $DATASET_DIR -n "${EXPERIMENT_NAME}_fold_1 -f "1" &> ${NOHUP}_fold_1" &
+
 nohup python3 main.py -i $DATA_DIR -g $GPU -t $DATASET_DIR -n "${EXPERIMENT_NAME}_fold_2 -f "2" &> ${NOHUP}_fold_2" &
+
 nohup python3 main.py -i $DATA_DIR -g $GPU -t $DATASET_DIR -n "${EXPERIMENT_NAME}_fold_3 -f "3" &> ${NOHUP}_fold_3" &
+
 nohup python3 main.py -i $DATA_DIR -g $GPU -t $DATASET_DIR -n "${EXPERIMENT_NAME}_fold_4 -f "4" &> ${NOHUP}_fold_4" &
