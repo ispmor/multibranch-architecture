d1ab21000acaad1f261db5d8901763035ebb3d90
Bartosz Puszkarski <bartosz.puszkarski@addepto.com>
2024-11-27 17:22:58 +0100

    running an experiment with baseline removal

--- run_fold_per_process.sh
+++ run_fold_per_process.sh
@@ -1,15 +1,11 @@
 DATA_DIR="/home/bartek/data/full_data_flat"
-EXPERIMENT_NAME="BW_NOT_REMOVED"
-GPU=2
-NOHUP="bw_not_removed"
-DATASET_DIR="h5_datasets/bw_not_removed/"
+EXPERIMENT_NAME="BW_REMOVED"
+GPU=1
+NOHUP="bw_removed"
+DATASET_DIR="h5_datasets/bw_removed/"
 
-nohup python3 main.py -i $DATA_DIR -g $GPU -t $DATASET_DIR -n "${EXPERIMENT_NAME}_fold_0" -f "0" &> "${NOHUP}_fold_0.out" &
-
-nohup python3 main.py -i $DATA_DIR -g $GPU -t $DATASET_DIR -n "${EXPERIMENT_NAME}_fold_1" -f "1" &> "${NOHUP}_fold_1.out" &
-
-nohup python3 main.py -i $DATA_DIR -g $GPU -t $DATASET_DIR -n "${EXPERIMENT_NAME}_fold_2" -f "2" &> "${NOHUP}_fold_2.out" &
-
-nohup python3 main.py -i $DATA_DIR -g $GPU -t $DATASET_DIR -n "${EXPERIMENT_NAME}_fold_3" -f "3" &> "${NOHUP}_fold_3.out" &
-
-nohup python3 main.py -i $DATA_DIR -g $GPU -t $DATASET_DIR -n "${EXPERIMENT_NAME}_fold_4" -f "4" &> "${NOHUP}_fold_4.out" &
+nohup python3 main.py -i $DATA_DIR -g $GPU -t $DATASET_DIR -n "${EXPERIMENT_NAME}_fold_0" -f "0" --remove-baseline &> "${NOHUP}_fold_0.out" &
+nohup python3 main.py -i $DATA_DIR -g $GPU -t $DATASET_DIR -n "${EXPERIMENT_NAME}_fold_1" -f "1" --remove-baseline &> "${NOHUP}_fold_1.out" &
+nohup python3 main.py -i $DATA_DIR -g $GPU -t $DATASET_DIR -n "${EXPERIMENT_NAME}_fold_2" -f "2" --remove-baseline &> "${NOHUP}_fold_2.out" &
+nohup python3 main.py -i $DATA_DIR -g $GPU -t $DATASET_DIR -n "${EXPERIMENT_NAME}_fold_3" -f "3" --remove-baseline &> "${NOHUP}_fold_3.out" &
+nohup python3 main.py -i $DATA_DIR -g $GPU -t $DATASET_DIR -n "${EXPERIMENT_NAME}_fold_4" -f "4" --remove-baseline &> "${NOHUP}_fold_4.out" &
