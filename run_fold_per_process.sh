eac8485cdf6d625189edf130d6307ec5ce0bea1f
Bartosz Puszkarski <bartosz.puszkarski@addepto.com>
2024-11-28 21:27:52 +0100

    adding more execution scripts

--- run_fold_per_process.sh
+++ run_fold_per_process.sh
@@ -1,11 +1,10 @@
 DATA_DIR="/home/bartek/data/full_data_flat"
 EXPERIMENT_NAME="BW_REMOVED"
 GPU=1
-NOHUP="bw_removed"
 DATASET_DIR="h5_datasets/bw_removed/"
 
-nohup python3 main.py -i $DATA_DIR -g $GPU -t $DATASET_DIR -n "${EXPERIMENT_NAME}_fold_0" -f "0" --remove-baseline &> "${NOHUP}_fold_0.out" &
-nohup python3 main.py -i $DATA_DIR -g $GPU -t $DATASET_DIR -n "${EXPERIMENT_NAME}_fold_1" -f "1" --remove-baseline &> "${NOHUP}_fold_1.out" &
-nohup python3 main.py -i $DATA_DIR -g $GPU -t $DATASET_DIR -n "${EXPERIMENT_NAME}_fold_2" -f "2" --remove-baseline &> "${NOHUP}_fold_2.out" &
-nohup python3 main.py -i $DATA_DIR -g $GPU -t $DATASET_DIR -n "${EXPERIMENT_NAME}_fold_3" -f "3" --remove-baseline &> "${NOHUP}_fold_3.out" &
-nohup python3 main.py -i $DATA_DIR -g $GPU -t $DATASET_DIR -n "${EXPERIMENT_NAME}_fold_4" -f "4" --remove-baseline &> "${NOHUP}_fold_4.out" &
+nohup python3 main.py -i $DATA_DIR -g $GPU -t $DATASET_DIR -n "${EXPERIMENT_NAME}_fold_0" -f "0" --remove-baseline &> "${EXPERIMENT_NAME}_fold_0.out" &
+nohup python3 main.py -i $DATA_DIR -g $GPU -t $DATASET_DIR -n "${EXPERIMENT_NAME}_fold_1" -f "1" --remove-baseline &> "${EXPERIMENT_NAME}_fold_1.out" &
+nohup python3 main.py -i $DATA_DIR -g $GPU -t $DATASET_DIR -n "${EXPERIMENT_NAME}_fold_2" -f "2" --remove-baseline &> "${EXPERIMENT_NAME}_fold_2.out" &
+nohup python3 main.py -i $DATA_DIR -g $GPU -t $DATASET_DIR -n "${EXPERIMENT_NAME}_fold_3" -f "3" --remove-baseline &> "${EXPERIMENT_NAME}_fold_3.out" &
+nohup python3 main.py -i $DATA_DIR -g $GPU -t $DATASET_DIR -n "${EXPERIMENT_NAME}_fold_4" -f "4" --remove-baseline &> "${EXPERIMENT_NAME}_fold_4.out" &
---
+++ run_training.sh
@@ -0,0 +1,7 @@
+DATA_DIR="/home/bartek/data/full_data_flat"
+EXPERIMENT_NAME="BW_REMOVED"
+GPU=1
+DATASET_DIR="h5_datasets/bw_removed/"
+
+
+nohup python3 main.py -i $DATA_DIR -g $GPU -t $DATASET_DIR -n "${EXPERIMENT_NAME}_training_and_test"  &> "${EXPERIMENT_NAME}_training_and_test.out" &
