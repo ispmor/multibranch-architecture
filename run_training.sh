bfc22d94c284c912a671a2d279f76e9cb8dda45f
Bartosz Puszkarski <bartosz.puszkarski@addepto.com>
2024-11-28 21:30:09 +0100

    prepping training for not removed bw

--- run_training.sh
+++ run_training.sh
@@ -1,7 +1,7 @@
 DATA_DIR="/home/bartek/data/full_data_flat"
-EXPERIMENT_NAME="BW_REMOVED"
+EXPERIMENT_NAME="BW_NOT_REMOVED"
 GPU=1
-DATASET_DIR="h5_datasets/bw_removed/"
+DATASET_DIR="h5_datasets/bw_not_removed/"
 
 
 nohup python3 main.py -i $DATA_DIR -g $GPU -t $DATASET_DIR -n "${EXPERIMENT_NAME}_training_and_test"  &> "${EXPERIMENT_NAME}_training_and_test.out" &
