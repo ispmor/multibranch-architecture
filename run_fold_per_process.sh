ca9926f1450d0b5ccc3d4a98330cbbeaf20189da
Bartosz Puszkarski <puszkarski.bartosz@gmail.com>
2024-12-02 19:59:46 +0000

    scripts are now better

--- run_fold_per_process.sh
+++ run_fold_per_process.sh
@@ -1,10 +1,13 @@
 DATA_DIR="/home/bartek/data/full_data_flat"
-EXPERIMENT_NAME="BW_REMOVED"
+LEADS=2
+EXPERIMENT_NAME="BW_REMOVED_$LEADS"
 GPU=1
 DATASET_DIR="h5_datasets/bw_removed/"
+FLAGS="--remove-baseline"
 
-nohup python3 main.py -i $DATA_DIR -g $GPU -t $DATASET_DIR -n "${EXPERIMENT_NAME}_fold_0" -f "0" --remove-baseline &> "${EXPERIMENT_NAME}_fold_0.out" &
-nohup python3 main.py -i $DATA_DIR -g $GPU -t $DATASET_DIR -n "${EXPERIMENT_NAME}_fold_1" -f "1" --remove-baseline &> "${EXPERIMENT_NAME}_fold_1.out" &
-nohup python3 main.py -i $DATA_DIR -g $GPU -t $DATASET_DIR -n "${EXPERIMENT_NAME}_fold_2" -f "2" --remove-baseline &> "${EXPERIMENT_NAME}_fold_2.out" &
-nohup python3 main.py -i $DATA_DIR -g $GPU -t $DATASET_DIR -n "${EXPERIMENT_NAME}_fold_3" -f "3" --remove-baseline &> "${EXPERIMENT_NAME}_fold_3.out" &
-nohup python3 main.py -i $DATA_DIR -g $GPU -t $DATASET_DIR -n "${EXPERIMENT_NAME}_fold_4" -f "4" --remove-baseline &> "${EXPERIMENT_NAME}_fold_4.out" &
+
+nohup python3 main.py -i $DATA_DIR -g $GPU -t $DATASET_DIR -n "${EXPERIMENT_NAME}_fold_0" -f "0" -l $LEADS $FLAGS &> "${EXPERIMENT_NAME}_fold_0.out" &
+nohup python3 main.py -i $DATA_DIR -g $GPU -t $DATASET_DIR -n "${EXPERIMENT_NAME}_fold_1" -f "1" -l $LEADS $FLAGS &> "${EXPERIMENT_NAME}_fold_1.out" &
+nohup python3 main.py -i $DATA_DIR -g $GPU -t $DATASET_DIR -n "${EXPERIMENT_NAME}_fold_2" -f "2" -l $LEADS $FLAGS &> "${EXPERIMENT_NAME}_fold_2.out" &
+nohup python3 main.py -i $DATA_DIR -g $GPU -t $DATASET_DIR -n "${EXPERIMENT_NAME}_fold_3" -f "3" -l $LEADS $FLAGS &> "${EXPERIMENT_NAME}_fold_3.out" &
+nohup python3 main.py -i $DATA_DIR -g $GPU -t $DATASET_DIR -n "${EXPERIMENT_NAME}_fold_4" -f "4" -l $LEADS $FLAGS &> "${EXPERIMENT_NAME}_fold_4.out" &
--- run_training.sh
+++ run_training.sh
@@ -1,7 +1,9 @@
 DATA_DIR="/home/bartek/data/full_data_flat"
-EXPERIMENT_NAME="BW_NOT_REMOVED"
-GPU=1
-DATASET_DIR="h5_datasets/bw_not_removed/"
+LEADS=2
+EXPERIMENT_NAME="BW_REMOVED_$LEADS"
+GPU=3
+DATASET_DIR="h5_datasets/bw_removed/"
+FLAGS="--remove-baseline"
 
 
-nohup python3 main.py -i $DATA_DIR -g $GPU -t $DATASET_DIR -n "${EXPERIMENT_NAME}_training_and_test"  &> "${EXPERIMENT_NAME}_training_and_test.out" &
+nohup python3 main.py -i $DATA_DIR -g $GPU -t $DATASET_DIR -n "${EXPERIMENT_NAME}_training_and_test" -l $LEADS $FLAGS &> "out/training/${EXPERIMENT_NAME}_training_and_test.out" &
