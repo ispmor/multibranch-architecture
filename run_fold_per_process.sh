16940cd1a9966801e8498ea70d5abdcdf86ec133
Bartosz Puszkarski <puszkarski.bartosz@gmail.com>
2025-01-15 22:40:36 +0000

    update

--- run_fold_per_process.sh
+++ run_fold_per_process.sh
@@ -1,13 +1,14 @@
 DATA_DIR="/home/bartek/data/full_data_flat"
-LEADS=2
-EXPERIMENT_NAME="BW_REMOVED_$LEADS"
-GPU=1
-DATASET_DIR="h5_datasets/bw_removed/"
-FLAGS="--remove-baseline"
+LEADS=12
+EXPERIMENT_NAME="NEW_DOMAIN_KNOWLEDGE_$LEADS"
+NETWORK="NBEATS"
+GPU=3
+DATASET_DIR="h5_datasets/NEW_DOMAIN_KNOWLEDGE/"
+FLAGS="--network ${NETWORK} --alpha-input-size 350 --beta-input-size 185 --gamma-input-size 2334 --delta-input-size 16 --epsilon-input-size 350 --zeta-input-size 350"
 
 
-nohup python3 main.py -i $DATA_DIR -g $GPU -t $DATASET_DIR -n "${EXPERIMENT_NAME}_fold_0" -f "0" -l $LEADS $FLAGS &> "${EXPERIMENT_NAME}_fold_0.out" &
-nohup python3 main.py -i $DATA_DIR -g $GPU -t $DATASET_DIR -n "${EXPERIMENT_NAME}_fold_1" -f "1" -l $LEADS $FLAGS &> "${EXPERIMENT_NAME}_fold_1.out" &
-nohup python3 main.py -i $DATA_DIR -g $GPU -t $DATASET_DIR -n "${EXPERIMENT_NAME}_fold_2" -f "2" -l $LEADS $FLAGS &> "${EXPERIMENT_NAME}_fold_2.out" &
-nohup python3 main.py -i $DATA_DIR -g $GPU -t $DATASET_DIR -n "${EXPERIMENT_NAME}_fold_3" -f "3" -l $LEADS $FLAGS &> "${EXPERIMENT_NAME}_fold_3.out" &
-nohup python3 main.py -i $DATA_DIR -g $GPU -t $DATASET_DIR -n "${EXPERIMENT_NAME}_fold_4" -f "4" -l $LEADS $FLAGS &> "${EXPERIMENT_NAME}_fold_4.out" &
+nohup python3 main.py -i $DATA_DIR -g $GPU -t $DATASET_DIR -n "${EXPERIMENT_NAME}" -f "0" -l $LEADS $FLAGS &> "out/data_prep/${EXPERIMENT_NAME}/fold_0.out" &
+nohup python3 main.py -i $DATA_DIR -g $GPU -t $DATASET_DIR -n "${EXPERIMENT_NAME}" -f "1" -l $LEADS $FLAGS &> "out/data_prep/${EXPERIMENT_NAME}/fold_1.out" &
+nohup python3 main.py -i $DATA_DIR -g $GPU -t $DATASET_DIR -n "${EXPERIMENT_NAME}" -f "2" -l $LEADS $FLAGS &> "out/data_prep/${EXPERIMENT_NAME}/fold_2.out" &
+nohup python3 main.py -i $DATA_DIR -g $GPU -t $DATASET_DIR -n "${EXPERIMENT_NAME}" -f "3" -l $LEADS $FLAGS &> "out/data_prep/${EXPERIMENT_NAME}/fold_3.out" &
+nohup python3 main.py -i $DATA_DIR -g $GPU -t $DATASET_DIR -n "${EXPERIMENT_NAME}" -f "4" -l $LEADS $FLAGS &> "out/data_prep/${EXPERIMENT_NAME}/fold_4.out" &
--- run_training.sh
+++ run_training.sh
@@ -1,9 +1,11 @@
 DATA_DIR="/home/bartek/data/full_data_flat"
-LEADS=2
-EXPERIMENT_NAME="BW_REMOVED_$LEADS"
-GPU=3
-DATASET_DIR="h5_datasets/bw_removed/"
-FLAGS="--remove-baseline"
-
+LEADS=12
+NETWORK="LSTM"
+COMMON="NEW_DOMAIN_KNOWLEDGE"
+EXPERIMENT_NAME="EPSILON_ONLY_${COMMON}_${LEADS}_${NETWORK}_7_2"
+GPU=2
+DATASET_DIR="h5_datasets/${COMMON}/"
+#FLAGS="--window-size 350 --wavelet-features-size 185 --network ${NETWORK} --alpha-hidden 7 --alpha-layers 2 --beta-hidden 7 --beta-layers 2 --alpha-input-size 350 --beta-input-size 185 --gamma-input-size 2298 --delta-input-size 10"
+FLAGS="--network ${NETWORK} --alpha-input-size 350 --beta-input-size 185 --gamma-input-size 2334 --delta-input-size 16 --epsilon-input-size 350 --zeta-input-size 350"
 
 nohup python3 main.py -i $DATA_DIR -g $GPU -t $DATASET_DIR -n "${EXPERIMENT_NAME}_training_and_test" -l $LEADS $FLAGS &> "out/training/${EXPERIMENT_NAME}_training_and_test.out" &
