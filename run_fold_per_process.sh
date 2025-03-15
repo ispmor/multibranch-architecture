68fd38c44da539f55b93c49c283cfa14f97728b8
Bartosz Puszkarski <puszkarski.bartosz@gmail.com>
2025-03-15 07:20:45 +0000

    making run script set proper size of network

--- run_fold_per_process.sh
+++ run_fold_per_process.sh
@@ -1,14 +1,14 @@
 DATA_DIR="/home/bartek/data/full_data_flat"
 LEADS=12
-EXPERIMENT_NAME="NEW_DOMAIN_KNOWLEDGE_$LEADS"
-NETWORK="NBEATS"
-GPU=3
-DATASET_DIR="h5_datasets/NEW_DOMAIN_KNOWLEDGE/"
-FLAGS="--network ${NETWORK} --alpha-input-size 350 --beta-input-size 185 --gamma-input-size 2334 --delta-input-size 16 --epsilon-input-size 350 --zeta-input-size 350"
+EXPERIMENT_NAME="3s_full"
+NETWORK="LSTM"
+GPU=1
+DATASET_DIR="h5_datasets/3s_full/"
+FLAGS="--network ${NETWORK} --alpha-input-size 1500 --beta-input-size 759 --gamma-input-size 10668 --delta-input-size 256 --epsilon-input-size 1500 --zeta-input-size 1500 --window-size 1500 --wavelet-features-size 759"
 
 
-nohup python3 main.py -i $DATA_DIR -g $GPU -t $DATASET_DIR -n "${EXPERIMENT_NAME}" -f "0" -l $LEADS $FLAGS &> "out/data_prep/${EXPERIMENT_NAME}/fold_0.out" &
-nohup python3 main.py -i $DATA_DIR -g $GPU -t $DATASET_DIR -n "${EXPERIMENT_NAME}" -f "1" -l $LEADS $FLAGS &> "out/data_prep/${EXPERIMENT_NAME}/fold_1.out" &
-nohup python3 main.py -i $DATA_DIR -g $GPU -t $DATASET_DIR -n "${EXPERIMENT_NAME}" -f "2" -l $LEADS $FLAGS &> "out/data_prep/${EXPERIMENT_NAME}/fold_2.out" &
-nohup python3 main.py -i $DATA_DIR -g $GPU -t $DATASET_DIR -n "${EXPERIMENT_NAME}" -f "3" -l $LEADS $FLAGS &> "out/data_prep/${EXPERIMENT_NAME}/fold_3.out" &
-nohup python3 main.py -i $DATA_DIR -g $GPU -t $DATASET_DIR -n "${EXPERIMENT_NAME}" -f "4" -l $LEADS $FLAGS &> "out/data_prep/${EXPERIMENT_NAME}/fold_4.out" &
+nohup python3 main.py -i $DATA_DIR -g $GPU -t $DATASET_DIR -n "${EXPERIMENT_NAME}_${LEADS}" -f "0" -l $LEADS $FLAGS &> "out/data_prep/${EXPERIMENT_NAME}/${LEADS}_fold_0.out" &
+nohup python3 main.py -i $DATA_DIR -g $GPU -t $DATASET_DIR -n "${EXPERIMENT_NAME}_${LEADS}" -f "1" -l $LEADS $FLAGS &> "out/data_prep/${EXPERIMENT_NAME}/${LEADS}_fold_1.out" &
+nohup python3 main.py -i $DATA_DIR -g $GPU -t $DATASET_DIR -n "${EXPERIMENT_NAME}_${LEADS}" -f "2" -l $LEADS $FLAGS &> "out/data_prep/${EXPERIMENT_NAME}/${LEADS}_fold_2.out" &
+nohup python3 main.py -i $DATA_DIR -g $GPU -t $DATASET_DIR -n "${EXPERIMENT_NAME}_${LEADS}" -f "3" -l $LEADS $FLAGS &> "out/data_prep/${EXPERIMENT_NAME}/${LEADS}_fold_3.out" &
+nohup python3 main.py -i $DATA_DIR -g $GPU -t $DATASET_DIR -n "${EXPERIMENT_NAME}_${LEADS}" -f "4" -l $LEADS $FLAGS &> "out/data_prep/${EXPERIMENT_NAME}/${LEADS}_fold_4.out" &
