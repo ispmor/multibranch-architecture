98081b95278b610d1653dcebf252c3d5d1fdebef
Bartosz Puszkarski <bartosz.puszkarski@addepto.com>
2025-06-05 17:15:53 +0200

    setup for experimet no epsilon branch

--- run_training.sh
+++ run_training.sh
@@ -5,8 +5,8 @@ ALPHA_HIDDEN="7"
 ALPHA_LAYERS="2"
 COMMON="3s_full"
 BRANCH_NAME=$(git symbolic-ref --short HEAD | sed -r 's/\//_/g') 
-EXPERIMENT_NAME="${BRANCH_NAME}_NO_ALPHA_${LEADS}_${NETWORK}_${ALPHA_HIDDEN}_${ALPHA_LAYERS}"
-GPU=3
+EXPERIMENT_NAME="${BRANCH_NAME}_NO_EPSILON_${LEADS}_${NETWORK}_${ALPHA_HIDDEN}_${ALPHA_LAYERS}"
+GPU=2
 DATASET_DIR="/data/NBEATS/h5_datasets/${COMMON}/"
 FLAGS="--network ${NETWORK} --epochs 30 --early-stop 5 --alpha-hidden ${ALPHA_HIDDEN} --alpha-layers ${ALPHA_LAYERS} --alpha-input-size 1500 --beta-input-size 759 --gamma-input-size 10668 --delta-input-size 256 --epsilon-input-size 1500 --zeta-input-size 1500 --window-size 1500 --wavelet-features-size 759"
 
