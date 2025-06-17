4934538bfd24511a59cb2d079d3865f58d7bc92e
Bartosz Puszkarski <puszkarski.bartosz@gmail.com>
2025-06-17 21:04:33 +0000

    reruning experiment for certainty

--- run_training.sh
+++ run_training.sh
@@ -1,13 +1,13 @@
 DATA_DIR="/home/bartek/data/full_data_flat"
 LEADS=12
-NETWORK="LSTM"
-ALPHA_HIDDEN="11"
+NETWORK="GRU"
+ALPHA_HIDDEN="7"
 ALPHA_LAYERS="2"
 COMMON="3s_full"
 BRANCH_NAME=$(git symbolic-ref --short HEAD | sed -r 's/\//_/g') 
 EXPERIMENT_NAME="${BRANCH_NAME}_6_BRANCHES_${COMMON}_${LEADS}_${NETWORK}_${ALPHA_HIDDEN}_${ALPHA_LAYERS}"
-GPU=3
-DATASET_DIR="h5_datasets/${COMMON}/"
+GPU=2
+DATASET_DIR="/data/NBEATS/h5_datasets/${COMMON}/"
 FLAGS="--network ${NETWORK} --epochs 30 --early-stop 5 --alpha-hidden ${ALPHA_HIDDEN} --alpha-layers ${ALPHA_LAYERS} --alpha-input-size 1500 --beta-input-size 759 --gamma-input-size 10668 --delta-input-size 256 --epsilon-input-size 1500 --zeta-input-size 1500 --window-size 1500 --wavelet-features-size 759"
 
 nohup python3 main.py -i $DATA_DIR -g $GPU -t $DATASET_DIR -n "${EXPERIMENT_NAME}_training_and_test" -l $LEADS $FLAGS &> "out/training/${EXPERIMENT_NAME}_training_and_test.out" &
