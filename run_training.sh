147b0ba4308018760839efe1b4bdf8bcf7925e1b
Bartosz Puszkarski <puszkarski.bartosz@gmail.com>
2025-07-27 11:34:26 +0000

    progrs on analysis

--- run_training.sh
+++ run_training.sh
@@ -6,7 +6,7 @@ ALPHA_LAYERS="2"
 COMMON="3s_full"
 BRANCH_NAME=$(git symbolic-ref --short HEAD | sed -r 's/\//_/g') 
 EXPERIMENT_NAME="${BRANCH_NAME}_6_BRANCHES_${COMMON}_${LEADS}_${NETWORK}_${ALPHA_HIDDEN}_${ALPHA_LAYERS}"
-GPU=2
+GPU=0
 DATASET_DIR="/data/NBEATS/h5_datasets/${COMMON}/"
 FLAGS="--network ${NETWORK} --epochs 30 --early-stop 5 --alpha-hidden ${ALPHA_HIDDEN} --alpha-layers ${ALPHA_LAYERS} --alpha-input-size 1500 --beta-input-size 759 --gamma-input-size 256 --delta-input-size 10668 --epsilon-input-size 1500 --zeta-input-size 1500 --window-size 1500 --wavelet-features-size 759 "
 
